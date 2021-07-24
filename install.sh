fceed3d185e98074cd74b60562c8e5acc81b5502
PeterDing <dfhayst@gmail.com>
2021-07-24 23:21:24 +0800

    Add apt -y

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ install_docker()
             ca-certificates \
             curl \
             gnupg \
-            lsb-release
+            lsb-release -y
 
         echo 'Add Docker’s official GPG key:'
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -29,7 +29,7 @@ install_docker()
           $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
         echo 'Install Docker'
-        sudo apt-get install docker-ce docker-ce-cli containerd.io
+        sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
         echo 'Run hello-world'
         sudo docker run hello-world
